da133d1da337e2425a20dd5f8c87fcccf4b2190e
Václav Oborník <vaclav.obornik@gmail.com>
2014-08-05 14:54:11 +0200

    repaired htmlMin

--- tasks/html2ts.js
+++ tasks/html2ts.js
@@ -15,73 +15,73 @@ var minify = require('html-minifier').minify;
 
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+    // Please see the Grunt documentation for more information regarding task
+    // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerTask('html2ts', 'The best Grunt plugin ever.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options();
+    grunt.registerTask('html2ts', 'The best Grunt plugin ever.', function() {
+        // Merge task-specific and/or target-specific options with these defaults.
+        var options = this.options();
 
-    /////////////////////////////////////////////////////////////////////
-    // HTML -> TS
-    ////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////////////////////
+        // HTML -> TS
+        ////////////////////////////////////////////////////////////////////
 
-    //html -> js processing functions:
-    // Originally from karma-html2js-preprocessor
-    // Refactored nicely in html2js grunt task
-    // https://github.com/karlgoldstein/grunt-html2js/blob/master/tasks/html2js.js
-    // Modified nlReplace to be an empty string
-    var escapeContent = function (content, quoteChar) {
-      var quoteRegexp = new RegExp('\\' + quoteChar, 'g');
-      var nlReplace = '';
-      return content.replace(quoteRegexp, '\\' + quoteChar).replace(/\r?\n/g, nlReplace);
-    };
+        //html -> js processing functions:
+        // Originally from karma-html2js-preprocessor
+        // Refactored nicely in html2js grunt task
+        // https://github.com/karlgoldstein/grunt-html2js/blob/master/tasks/html2js.js
+        // Modified nlReplace to be an empty string
+        var escapeContent = function (content, quoteChar) {
+            var quoteRegexp = new RegExp('\\' + quoteChar, 'g');
+            var nlReplace = '';
+            return content.replace(quoteRegexp, '\\' + quoteChar).replace(/\r?\n/g, nlReplace);
+        };
 
-    // Remove bom when reading utf8 files
-    function stripBOM(str) {
-      return 0xFEFF == str.charCodeAt(0)
-        ? str.substring(1)
-        : str;
-    }
-    var htmlTemplate = _.template("module <%= modulename %> { export var <%= varname %> =  '<%= content %>'; } ");
-
-
-    compileHTML(grunt.option('html2ts_file'), grunt.option('html2ts_base_dir'));
-
-    // Compile an HTML file to a TS file
-    // Return the filename. This filename will be required by reference.ts
-    function compileHTML(filename, baseDir) {
-
-      var htmlContent = escapeContent(fs.readFileSync(filename).toString(), "'");
-      if(grunt.option('hmtlmin')) {
-          htmlContent = minify(htmlContent, grunt.option('hmtlmin'));
-      }
-
-      var dirName;
-      if(baseDir) {
-        dirName = path.dirname(path.relative(baseDir, filename));
-      } else {
-        dirName = path.dirname(filename);
-      }
-      var moduleName = dirName.replace(/[\\\/]/g, '.') + '.template';
-      moduleName = moduleName.replace('directives', 'directive');
-      moduleName = moduleName.replace('services', 'service');
-      moduleName = moduleName.replace('controllers', 'controller');
-      moduleName = moduleName.replace('filters', 'filter');
-      moduleName = moduleName.replace('modals', 'modal');
-
-      var ext = path.extname(filename);
-      var extFreeName = path.basename(filename, ext).match(/^[a-zA-Z\-_]+/g)[0];
-      var fileContent = htmlTemplate({ modulename: moduleName, varname: extFreeName, content: htmlContent });
-
-      // Write the content to a file
-      var outputfile = filename + ".ts";
-
-      fs.writeFileSync(outputfile, fileContent);
-      return outputfile;
-    }
+        // Remove bom when reading utf8 files
+        function stripBOM(str) {
+            return 0xFEFF == str.charCodeAt(0)
+                ? str.substring(1)
+                : str;
+        }
+        var htmlTemplate = _.template("module <%= modulename %> { export var <%= varname %> =  '<%= content %>'; } ");
 
 
-  });
+        compileHTML(grunt.option('html2ts_file'), grunt.option('html2ts_base_dir'));
+
+        // Compile an HTML file to a TS file
+        // Return the filename. This filename will be required by reference.ts
+        function compileHTML(filename, baseDir) {
+
+            var htmlContent = escapeContent(fs.readFileSync(filename).toString(), "'");
+            if(options.htmlMin) {
+                htmlContent = minify(htmlContent, options.htmlMin);
+            }
+
+            var dirName;
+            if(baseDir) {
+                dirName = path.dirname(path.relative(baseDir, filename));
+            } else {
+                dirName = path.dirname(filename);
+            }
+            var moduleName = dirName.replace(/[\\\/]/g, '.') + '.template';
+            moduleName = moduleName.replace('directives', 'directive');
+            moduleName = moduleName.replace('services', 'service');
+            moduleName = moduleName.replace('controllers', 'controller');
+            moduleName = moduleName.replace('filters', 'filter');
+            moduleName = moduleName.replace('modals', 'modal');
+
+            var ext = path.extname(filename);
+            var extFreeName = path.basename(filename, ext).match(/^[a-zA-Z\-_]+/g)[0];
+            var fileContent = htmlTemplate({ modulename: moduleName, varname: extFreeName, content: htmlContent });
+
+            // Write the content to a file
+            var outputfile = filename + ".ts";
+
+            fs.writeFileSync(outputfile, fileContent);
+            return outputfile;
+        }
+
+
+    });
 
 };
